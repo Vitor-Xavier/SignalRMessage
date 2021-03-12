0b01d58dd16cd0c1c75dab0f4e3a60b085a0eb95
Vitor Xavier de Souza <vitorvxs@live.com>
2021-03-12 19:46:11 -0300

    Add client webapp

---
+++ src/SignalRMessage.WebApp/Crosscutting/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SignalRMessage.WebApp.Crosscutting
+{
+    public class Message
+    {
+        public string Content { get; set; }
+
+        public string To { get; set; }
+
+        public DateTime ReceivedDateTime { get; set; }
+    }
+}
---
+++ src/SignalRMessage.WebApp/Crosscutting/UserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SignalRMessage.WebApp.Crosscutting
+{
+    public class UserDto
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Token { get; set; }
+    }
+}
